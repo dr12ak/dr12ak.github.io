b9b877695e35894e5f76377c1f6f3d0055286bf3
dr12ak <148722760+dr12ak@users.noreply.github.com>
2023-11-02 21:33:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let abort = false;
+let downloadCounter = 0;
+
 function getClass(element) {
   return element.className.substring(0, 2);
 }
@@ -29,7 +32,7 @@ function addTab(element) {
 
   const article = document.createElement("article");
   article.classList.add("q" + element.dataset.index);
-  article.innerHTML = '<textarea class="prompt" rows="10"></textarea><div class="controls"><button onclick="queueQuery(this);">Start</button><div><input class="iterations" type="number" min="1" max="100" step="1" value="20" /><div class="progress">0/0</div></div></div><details><summary>Negative Prompt</summary><textarea rows="5"></textarea></details>';
+  article.innerHTML = '<textarea class="prompt" rows="10" spellcheck="false">(masterpiece, best quality, ultra-detailed), (illustration), (beautiful detailed eyes), (very detailed face), (1girl), (solo), (depth of field), bokeh, light smile, full-face blush, (open mouth:0.6), sweat, sweatdrop, thighs, looking at viewer, sunlight, dappled sunlight, (large breasts:1), </textarea><div class="controls"><button onclick="queueQuery(this);">Start</button><div><input class="iterations" type="number" min="1" max="100" step="1" value="20" /><div class="progress">0/0</div></div></div><details><summary>Negative Prompt</summary><textarea class="negative-prompt" rows="5" spellcheck="false">(painting by bad-artist-anime:0.9), (painting by bad-artist:0.9), bad-hands-5, watermark, text, error, blurry, jpeg artifacts, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, artist name, (worst quality, low quality:1.4), bad anatomy, ((cleavage)), (collar), (seiza), (wariza), badhandv4</textarea></details>';
   document.querySelector("main").append(article);
 
   element.dataset.index = parseInt(element.dataset.index) + 1;
@@ -39,6 +42,12 @@ function queueQuery(element) {
   const tab = document.querySelector("li." + getClass(element.parentElement.parentElement));
   tab.classList.toggle("queued");
   if (tab.classList.contains("queued")) {
+    if (element.innerHTML === "Stopping") {
+      abort = false;
+      element.innerHTML = "Stop";
+      return;
+    }
+    document.querySelector("article." + getClass(tab) + " .progress").innerHTML = "0/" + parseInt(document.querySelector("article." + getClass(tab) + " .iterations").value);
     let max = -1;
     document.querySelectorAll(".queued").forEach((element) => {
       if (max < parseInt(element.dataset.order)) max = parseInt(element.dataset.order);
@@ -53,15 +62,27 @@ function queueQuery(element) {
       createSet(getClass(tab));
     }
   } else {
-    tab.dataset.order = "-1";
-    element.classList.remove("can-stop");
-    element.innerHTML = "Start";
+    if (tab.classList.contains("running")) {
+      abort = true;
+      element.innerHTML = "Stopping";
+    } else {
+      tab.dataset.order = "-1";
+      element.classList.remove("can-stop");
+      element.innerHTML = "Start";
+    }
   }
 }
 
 async function createSet(queryClass) {
   const url = document.querySelector("#url").value;
-  const iterations = parseInt(document.querySelector("article.active .iterations").value);
+  const iterations = parseInt(document.querySelector("article." + queryClass + " .iterations").value);
+  let prompt = document.querySelector("article." + queryClass + " .prompt").value;
+  console.log(prompt);
+  prompt = dynamicPrompt(prompt);
+  console.log(prompt);
+  let negativePrompt = document.querySelector("article." + queryClass + " .negative-prompt").value;
+  prompt = dynamicPrompt(negativePrompt);
+
   const payload = {
     enable_hr: true,
     denoising_strength: 0.6,
@@ -77,82 +98,84 @@ async function createSet(queryClass) {
     width: 448,
     height: 640,
     CLIP_stop_at_last_layers: 2,
-    prompt: "(masterpiece, best quality, ultra-detailed), (illustration), (beautiful detailed eyes), (very detailed face), (1girl), (solo), march seventh, blue eyes, hair between eyes, medium hair, pink eyes, pink hair, two-tone eyes, (depth of field), bokeh, light smile, full-face blush, (open mouth:0.6), sweat, sweatdrop, thighs, looking at viewer, sunlight, dappled sunlight, (large breasts:1), beach, ocean, bikini, <lora:marchseventh-lora-nochekaiser:0.9>",
-    negative_prompt: "(painting by bad-artist-anime:0.9), (painting by bad-artist:0.9), bad-hands-5, watermark, text, error, blurry, jpeg artifacts, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, artist name, (worst quality, low quality:1.4), bad anatomy, ((cleavage)), (collar), (seiza), (wariza), badhandv4,",
+    prompt: prompt,
+    negative_prompt: negativePrompt,
   };
 
-  let prompt = "(masterpiece, best quality, ultra-detailed), (illustration), (beautiful detailed eyes), (very detailed face), (1girl), (solo), (depth of field), bokeh, light smile, full-face blush, (open mouth:0.6), sweat, sweatdrop, thighs, looking at viewer, sunlight, dappled sunlight, (large breasts:1)";
-  let promptAddOn = document.querySelector("article.active .prompt").value;
-  payload["prompt"] = prompt + promptAddOn;
-
-  await running(url, iterations, payload);
-
-  /*
-  !mkdir {'/content/' + folder}
-  for i in range(iterations):
-    newPrompt = OR(prompt)
-    newPrompt = OR(newPrompt)
-    newPrompt = SPLIT(newPrompt)
-    newPrompt = CHANCE(newPrompt)
-    print(newPrompt)
-    payload["prompt"] = newPrompt
-    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
-    r = response.json()
-
-    for j in range(len(r['images'])):
-      image = Image.open(io.BytesIO(base64.b64decode(r['images'][j])))
-      image.save('/content/' + folder + '/' + folder[0] + chr(97 + j)+ str(i) +'.png')
-  */
-
-  /*let worker = new Worker(
-    `data:text/javascript,
-    const payload = {
-      enable_hr: True,
-      denoising_strength: 0.6,
-      hr_upscaler: "Latent (nearest-exact)",
-      hr_second_pass_steps: 20,
-      hr_resize_x: 960,
-      hr_resize_y: 1280,
-      seed: -1,
-      sampler_name: "DPM++ 2M Karras",
-      batch_size: 2,
-      steps: 25,
-      cfg_scale: 7.5,
-      width: 448,
-      height: 640,
-      CLIP_stop_at_last_layers: 2,
-      prompt: "(masterpiece, best quality, ultra-detailed), (illustration), (beautiful detailed eyes), (very detailed face), (1girl), (solo), march seventh, blue eyes, hair between eyes, medium hair, pink eyes, pink hair, two-tone eyes, (depth of field), bokeh, light smile, full-face blush, (open mouth:0.6), sweat, sweatdrop, thighs, looking at viewer, sunlight, dappled sunlight, (large breasts:1), beach, ocean, bikini, <lora:marchseventh-lora-nochekaiser:0.9>",
-      negative_prompt: "(painting by bad-artist-anime:0.9), (painting by bad-artist:0.9), bad-hands-5, watermark, text, error, blurry, jpeg artifacts, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, artist name, (worst quality, low quality:1.4), bad anatomy, ((cleavage)), (collar), (seiza), (wariza), badhandv4,",
-    };
-        for (let i = 0; i < ${iterations}; i++) {
-          fetch(${url} + "/sdapi/v1/txt2img", {
-            method: "POST",
-            body: JSON.stringify(payload),
-            headers: {
-              "Content-type": "application/json; charset=UTF-8",
-            },
-          })
-            .then((response) => response.json())
-            .then((json) => console.log(json););
-        }
-        `
-  );
-  /*worker.onmessage = function (event) {
-    //Get the result from the worker. This code will be called when postMessage is called in the worker.
-    alert("The result is " + event.data);
-  };*/
+  for (let i = 0; i < iterations; i++) {
+    if (abort) {
+      downloadCounter++;
+      startNextQuery(queryClass);
+      return;
+    }
+    console.log(payload["prompt"]);
+    const response = await fetch(`${url}/sdapi/v1/txt2img`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
+    const json = await response.json();
+    json.images.forEach((image, index) => {
+      let a = document.createElement("a");
+      a.href = "data:image/png;base64," + image;
+      a.download = `${String.fromCharCode("a".charCodeAt(0) + downloadCounter) + String.fromCharCode("a".charCodeAt(0) + index) + i}.png`;
+      a.click();
+    });
+    document.querySelector("article." + queryClass + " .progress").innerHTML = parseInt(i + 1) + "/" + iterations;
+  }
+  downloadCounter++;
+  startNextQuery(queryClass);
 }
 
-async function running(url, iterations, payload) {
-  for (let i = 0; i < iterations; i++) {
-    fetch(url + "/sdapi/v1/txt2img", {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+function dynamicPrompt(prompt) {
+  Array.from(prompt.matchAll(/{([^{]+?)}/g)).forEach((group) => {
+    let weightedArray = [];
+    let items = Array.from(group[1].matchAll(/([^\|:]+):?([0-9.]*)/g));
+    if (items.length === 1) {
+      if (items[0][2] && /\S/.test(items[0][2])) items.push(["", "", (1 - parseFloat(items[0][2])).toString()]);
+      else items.push(["", "", "0"]);
+    }
+    items.forEach((item) => {
+      if (item[2] && /\S/.test(item[2])) weightedArray.push(parseFloat(item[2]));
+      else weightedArray.push(1);
+    });
+    prompt = prompt.replace(group[0], items[weightedRandom(weightedArray)][1].trim());
+  });
+  return prompt;
+}
+
+function weightedRandom(weightedArray) {
+  let table = [];
+  for (let i = 0; i < weightedArray.length; i++) {
+    let multiplier = 10 ^ (weightedArray[i] % 1 === 0 ? 0 : weightedArray[i].toString().split(".")[1].length);
+    for (let j = 0; j < weightedArray[i] * multiplier; j++) {
+      table.push(i);
+    }
+  }
+  return table[Math.floor(Math.random() * table.length)];
+}
+
+function startNextQuery(queryClass) {
+  abort = false;
+  const currentTab = document.querySelector("li." + queryClass);
+  currentTab.dataset.order = "-1";
+  currentTab.classList.remove("running");
+  currentTab.classList.remove("queued");
+  const button = document.querySelector("article." + queryClass + " button");
+  button.classList.remove("can-stop");
+  button.innerHTML = "Start";
+  let order = Number.MAX_SAFE_INTEGER;
+  let nextQuery = null;
+  let tabs = document.querySelectorAll("li.queued");
+  tabs.forEach((tab) => {
+    if (order > parseInt(tab.dataset.order)) {
+      order = parseInt(tab.dataset.order);
+      nextQuery = tab;
+    }
+  });
+  if (nextQuery != null) {
+    nextQuery.classList.add("running");
+    document.querySelector("article." + getClass(nextQuery) + " button").innerHTML = "Stop";
+    createSet(getClass(nextQuery));
   }
 }
---
+++ autocomplete/autocomplete.js
@@ -0,0 +1,383 @@
+// From https://github.com/component/textarea-caret-position
+
+// We'll copy the properties below into the mirror div.
+// Note that some browsers, such as Firefox, do not concatenate properties
+// into their shorthand (e.g. padding-top, padding-bottom etc. -> padding),
+// so we have to list every single property explicitly.
+var properties = [
+  "direction", // RTL support
+  "boxSizing",
+  "width", // on Chrome and IE, exclude the scrollbar, so the mirror div wraps exactly as the textarea does
+  "height",
+  "overflowX",
+  "overflowY", // copy the scrollbar for IE
+
+  "borderTopWidth",
+  "borderRightWidth",
+  "borderBottomWidth",
+  "borderLeftWidth",
+  "borderStyle",
+
+  "paddingTop",
+  "paddingRight",
+  "paddingBottom",
+  "paddingLeft",
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/font
+  "fontStyle",
+  "fontVariant",
+  "fontWeight",
+  "fontStretch",
+  "fontSize",
+  "fontSizeAdjust",
+  "lineHeight",
+  "fontFamily",
+
+  "textAlign",
+  "textTransform",
+  "textIndent",
+  "textDecoration", // might not make a difference, but better be safe
+
+  "letterSpacing",
+  "wordSpacing",
+
+  "tabSize",
+  "MozTabSize",
+];
+
+var isBrowser = typeof window !== "undefined";
+var isFirefox = isBrowser && window.mozInnerScreenX != null;
+
+function getCaretCoordinates(element, position, options) {
+  if (!isBrowser) {
+    throw new Error("textarea-caret-position#getCaretCoordinates should only be called in a browser");
+  }
+
+  var debug = (options && options.debug) || false;
+  if (debug) {
+    var el = document.querySelector("#input-textarea-caret-position-mirror-div");
+    if (el) el.parentNode.removeChild(el);
+  }
+
+  // The mirror div will replicate the textarea's style
+  var div = document.createElement("div");
+  div.id = "input-textarea-caret-position-mirror-div";
+  document.body.appendChild(div);
+
+  var style = div.style;
+  var computed = window.getComputedStyle ? window.getComputedStyle(element) : element.currentStyle; // currentStyle for IE < 9
+  var isInput = element.nodeName === "INPUT";
+
+  // Default textarea styles
+  style.whiteSpace = "pre-wrap";
+  if (!isInput) style.wordWrap = "break-word"; // only for textarea-s
+
+  // Position off-screen
+  style.position = "absolute"; // required to return coordinates properly
+  if (!debug) style.visibility = "hidden"; // not 'display: none' because we want rendering
+
+  // Transfer the element's properties to the div
+  properties.forEach(function (prop) {
+    if (isInput && prop === "lineHeight") {
+      // Special case for <input>s because text is rendered centered and line height may be != height
+      if (computed.boxSizing === "border-box") {
+        var height = parseInt(computed.height);
+        var outerHeight = parseInt(computed.paddingTop) + parseInt(computed.paddingBottom) + parseInt(computed.borderTopWidth) + parseInt(computed.borderBottomWidth);
+        var targetHeight = outerHeight + parseInt(computed.lineHeight);
+        if (height > targetHeight) {
+          style.lineHeight = height - outerHeight + "px";
+        } else if (height === targetHeight) {
+          style.lineHeight = computed.lineHeight;
+        } else {
+          style.lineHeight = 0;
+        }
+      } else {
+        style.lineHeight = computed.height;
+      }
+    } else {
+      style[prop] = computed[prop];
+    }
+  });
+
+  if (isFirefox) {
+    // Firefox lies about the overflow property for textareas: https://bugzilla.mozilla.org/show_bug.cgi?id=984275
+    if (element.scrollHeight > parseInt(computed.height)) style.overflowY = "scroll";
+  } else {
+    style.overflow = "hidden"; // for Chrome to not render a scrollbar; IE keeps overflowY = 'scroll'
+  }
+
+  div.textContent = element.value.substring(0, position);
+  // The second special handling for input type="text" vs textarea:
+  // spaces need to be replaced with non-breaking spaces - http://stackoverflow.com/a/13402035/1269037
+  if (isInput) div.textContent = div.textContent.replace(/\s/g, "\u00a0");
+
+  var span = document.createElement("span");
+  // Wrapping must be replicated *exactly*, including when a long word gets
+  // onto the next line, with whitespace at the end of the line before (#7).
+  // The  *only* reliable way to do that is to copy the *entire* rest of the
+  // textarea's content into the <span> created at the caret position.
+  // For inputs, just '.' would be enough, but no need to bother.
+  span.textContent = element.value.substring(position) || "."; // || because a completely empty faux span doesn't render at all
+  div.appendChild(span);
+
+  var coordinates = {
+    top: span.offsetTop + parseInt(computed["borderTopWidth"]),
+    left: span.offsetLeft + parseInt(computed["borderLeftWidth"]),
+    height: parseInt(computed["lineHeight"]),
+  };
+
+  if (debug) {
+    span.style.backgroundColor = "#aaa";
+  } else {
+    document.body.removeChild(div);
+  }
+
+  return coordinates;
+}
+
+/*
+
+
+
+
+
+*/
+//get object from csv
+function parseCSV(str) {
+  const arr = [];
+  let quote = false; // 'true' means we're inside a quoted field
+
+  // Iterate over each character, keep track of current row and column (of the returned array)
+  for (let row = 0, col = 0, c = 0; c < str.length; c++) {
+    let cc = str[c],
+      nc = str[c + 1]; // Current character, next character
+    arr[row] = arr[row] || []; // Create a new row if necessary
+    arr[row][col] = arr[row][col] || ""; // Create a new column (start with empty string) if necessary
+
+    // If the current character is a quotation mark, and we're inside a
+    // quoted field, and the next character is also a quotation mark,
+    // add a quotation mark to the current column and skip the next character
+    if (cc == '"' && quote && nc == '"') {
+      arr[row][col] += cc;
+      ++c;
+      continue;
+    }
+
+    // If it's just one quotation mark, begin/end quoted field
+    if (cc == '"') {
+      quote = !quote;
+      continue;
+    }
+
+    // If it's a comma and we're not in a quoted field, move on to the next column
+    if (cc == "," && !quote) {
+      ++col;
+      continue;
+    }
+
+    // If it's a newline (CRLF), skip the next character and move on to the next row and move to column 0 of that new row
+    if (cc == "\r" && nc == "\n") {
+      ++row;
+      col = 0;
+      ++c;
+      quote = false;
+      continue;
+    }
+
+    // If it's a newline (LF or CR) move on to the next row and move to column 0 of that new row
+    if (cc == "\n") {
+      ++row;
+      col = 0;
+      quote = false;
+      continue;
+    }
+    if (cc == "\r") {
+      ++row;
+      col = 0;
+      quote = false;
+      continue;
+    }
+
+    // Otherwise, append the current character to the current column
+    arr[row][col] += cc;
+  }
+  return arr;
+}
+
+async function loadCSV(path) {
+  let response = await fetch(path);
+  let text = await response.text();
+  return parseCSV(text);
+}
+
+/*
+
+
+
+
+*/
+// autocomplete div
+let allTags = [];
+let autocompleteTextarea;
+
+window.onload = async () => {
+  allTags = await loadCSV("autocomplete/tags1.csv");
+
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "autocomplete");
+  ul.innerHTML = '<li class="tag-autocomplete empty-autocomplete"></li><li class="tag-autocomplete empty-autocomplete"></li><li class="tag-autocomplete empty-autocomplete"></li><li class="tag-autocomplete empty-autocomplete"></li><li class="tag-autocomplete empty-autocomplete"></li>';
+  document.body.append(ul);
+
+  document.querySelector("#tab-settings").addEventListener("input", (event) => {
+    if (event.target.closest("textarea")) onInput(event.target.closest("textarea"), event);
+  });
+
+  document.querySelector("#tab-settings").addEventListener("keydown", (event) => {
+    if (event.target.closest("textarea")) onKeyDown(event.target.closest("textarea"), event);
+  });
+  document.addEventListener("click", (event) => {
+    if (event.target.closest("#autocomplete")) insertTag(event.target.closest(".tag-autocomplete"));
+    else document.querySelectorAll(".tag-autocomplete").forEach((item) => item.classList.add("empty-autocomplete"));
+  });
+};
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+function onInput(element, event) {
+  autocompleteTextarea = getWord(element);
+  let word = autocompleteTextarea.sanitizedWord;
+  if (word && /\S/.test(word)) {
+    let caretPosition = getCaretCoordinates(element, element.selectionEnd);
+    const autocomplete = document.querySelector("#autocomplete");
+    autocomplete.style.left = parseInt(element.offsetLeft - element.scrollLeft + caretPosition.left) + "px";
+    autocomplete.style.top = parseInt(element.offsetTop - element.scrollTop + caretPosition.top + parseInt(getComputedStyle(element).lineHeight)) + "px";
+
+    //const searchRegex = new RegExp(escapeRegExp(word) + "([^/]*\\/?)", "i");
+
+    const results = searchTags(word);
+    document.querySelectorAll(".tag-autocomplete").forEach((item, index) => {
+      if (results.length > index) {
+        let innerHTML;
+        if (results[index][0].includes(word)) innerHTML = results[index][0] + '<span class="tag-popularity">' + nFormatter(results[index][2], 0) + "</span>";
+        else {
+          results[index][3].split(",").forEach((alternativeTag) => {
+            if (alternativeTag.includes(word)) innerHTML = alternativeTag + " → " + results[index][0] + '<span class="tag-popularity">' + nFormatter(results[index][2], 0) + "</span>";
+          });
+        }
+        item.innerHTML = innerHTML;
+        item.classList.remove("empty-autocomplete");
+      } else {
+        item.innerHTML = "";
+        item.classList.add("empty-autocomplete");
+      }
+    });
+  } else document.querySelectorAll(".tag-autocomplete").forEach((item) => item.classList.add("empty-autocomplete"));
+}
+
+function nFormatter(num, digits) {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+}
+
+function getWord(element) {
+  let left = element.selectionStart;
+  if (element.value[left] === ",") left--;
+  while (left >= 0 && element.value[left] !== ",") {
+    left--;
+  }
+  let right = element.selectionEnd - 1;
+  if (element.value[right] === ",") right++;
+  while (right <= element.value.length - 1 && element.value[right] !== ",") {
+    right++;
+  }
+
+  let originalWord = element.value.substring(left + 1, right);
+  let sanitizedWord = originalWord.trim().replaceAll(" ", "_").replaceAll("\\(", "(").replaceAll("\\)", ")").replaceAll("\\[", "[").replaceAll("\\]", "]");
+
+  return { textarea: element, originalWord: originalWord, sanitizedWord: sanitizedWord, index: left + 1 };
+}
+
+function searchTags(word) {
+  let count = 0;
+  let results = [];
+  /*const results = allTags.filter((tag) => {
+    if (count < 5 && (tag[0].toLowerCase().includes(word) || tag[3].toLowerCase().includes(word))) {
+      count++;
+      return true;
+    }
+    return false;
+  });*/
+
+  for (let i = 0; i < allTags.length; i++) {
+    if (count >= 5) break;
+    if (allTags[i][0].toLowerCase().includes(word) || allTags[i][3].toLowerCase().includes(word)) {
+      results.push(allTags[i]);
+      count++;
+    }
+  }
+
+  return results;
+}
+
+function onKeyDown(element, event) {
+  if ((event.key === "ArrowDown" || event.key === "ArrowUp") && document.querySelectorAll(".empty-autocomplete").length < document.querySelectorAll(".tag-autocomplete").length) {
+    event.preventDefault();
+    const items = document.querySelectorAll(".tag-autocomplete");
+    let i = -1;
+    items.forEach((item, index) => {
+      if (item.classList.contains("selected-tag-autocomplete")) {
+        i = index;
+        item.classList.remove("selected-tag-autocomplete");
+      }
+    });
+    if (event.key === "ArrowDown") i++;
+    else if (event.key == "ArrowUp") i--;
+    let visibleItems = [];
+    for (let i = 0; i < items.length; i++) {
+      if (!items[i].classList.contains("empty-autocomplete")) visibleItems.push(items[i]);
+    }
+    visibleItems[((i % visibleItems.length) + visibleItems.length) % visibleItems.length].classList.add("selected-tag-autocomplete");
+  } else if (event.key === "Enter") {
+    event.preventDefault();
+    insertTag(document.querySelector(".selected-tag-autocomplete"));
+  } else if (event.key === "Tab") {
+    event.preventDefault();
+    if (!document.querySelector(".selected-tag-autocomplete")) document.querySelectorAll(".tag-autocomplete")[0].classList.add("selected-tag-autocomplete");
+    insertTag(document.querySelector(".selected-tag-autocomplete"));
+  }
+}
+
+function insertTag(element) {
+  if (element) {
+    let result = element.firstChild.textContent
+      .slice(element.firstChild.textContent.lastIndexOf(" ") - element.firstChild.textContent.length)
+      .trim()
+      .replaceAll("_", " ")
+      .replaceAll("(", "\\(")
+      .replaceAll(")", "\\)")
+      .replaceAll("[", "\\[")
+      .replaceAll("]", "\\]");
+    autocompleteTextarea.textarea.value = autocompleteTextarea.textarea.value.substring(0, autocompleteTextarea.index) + " " + result + "," + autocompleteTextarea.textarea.value.substring(autocompleteTextarea.index + autocompleteTextarea.originalWord.length);
+    autocompleteTextarea.textarea.value = autocompleteTextarea.textarea.value.replaceAll(",,", ",");
+    autocompleteTextarea.textarea.selectionEnd = autocompleteTextarea.index + result.length + 2;
+    document.querySelectorAll(".tag-autocomplete").forEach((item) => item.classList.add("empty-autocomplete"));
+    document.querySelector(".selected-tag-autocomplete").classList.remove("selected-tag-autocomplete");
+  }
+}
