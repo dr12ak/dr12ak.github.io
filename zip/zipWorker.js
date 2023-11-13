7f81a6d22b667594850a25bf6ce26ce01e794dfa
dr12ak <148722760+dr12ak@users.noreply.github.com>
2023-11-13 22:28:41 +0100

    Update zipWorker.js

--- zip/zipWorker.js
+++ zip/zipWorker.js
@@ -6,8 +6,7 @@ onmessage = async (e) => {
   if (e.data.file) files.push(new File([await (await fetch(e.data.file)).blob()], e.data.filename, { type: "image/png" }));
   else {
     zipURL = URL.createObjectURL(await downloadZip(files).blob());
-    if (e.data.isApp) gonative.share.downloadFile({ url: zipURL });
-    else postMessage({ url: zipURL });
+    postMessage({ url: zipURL });
     self.close();
   }
 };
